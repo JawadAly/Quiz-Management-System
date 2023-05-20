979676a30b571dccda0e02e5147ad36866306e60
jawadSaleem <jawadsaleem932@gmail.com>
2023-05-21 01:32:59 +0500

    Initial commit plus implemented student registration functionality

---
+++ src/controllers/stdCrudInterface.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package controllers;
+
+import models.Student;
+
+/**
+ *
+ * @author Jawad Saleem
+ */
+public interface stdCrudInterface {
+    public void stdInsert(Student std);
+//    public void stdUpdatePass(Student std);
+//    public void stdStruckOff(Student std);
+//    public List<Student> getRegtdStdntsList();
+}
---
+++ src/dbConnection/databaseConnection.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dbConnection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Jawad Saleem
+ */
+public class databaseConnection {
+      static Connection conn; 
+    static String driver = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+    static String url = "jdbc:sqlserver://localhost\\SQLEXPRESS:1433;databaseName=quiz_management_db;encrypt=true;trustServerCertificate=true";
+    static String userName = "jawad";
+    static String pass = "jawad";
+    public static Connection getConnection() throws SQLException, ClassNotFoundException{
+        if(conn == null){
+             //registering jdbc driver
+        Class.forName(driver);
+        //establishing connection through driver
+        conn = DriverManager.getConnection(url,userName,pass);
+        }
+       
+        return conn;
+    }
+    
+    //code to test connection
+    
+//    public static void main(String []args){
+//        try{
+//        databaseConnection con = new databaseConnection();
+//        System.out.println("Connection made");
+//        }
+//        catch(Exception e){
+//            System.out.println("Connection not made");
+//        }
+//        
+//        
+//    }
+}
